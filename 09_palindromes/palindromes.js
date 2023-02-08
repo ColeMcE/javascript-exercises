6267d00e97db31bf2f9bae017fea9a656081dc0b
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-02-08 11:28:04 -0600

    Up to and including Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let lowerCase = input.toLowerCase();
+    let punctuationLess = lowerCase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let finalString = punctuationLess.replace(/\s/g,"");
+    function reverseString (finalString) {
+        return finalString.split("").reverse().join("");
+    }
+    let checkString = reverseString(finalString);
+    if (finalString == checkString) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
 
 };
 
