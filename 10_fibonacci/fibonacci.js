a92bccc59529d701c1cf14c54e0ea2341ab21da1
ColeMcE <mcelfresh.cole@gmail.com>
2023-02-08 19:09:54 -0600

    Completed Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNumb) {
+    const fibonacciArray = [];
+    let n1 = 0, n2 = 1, nextTerm;
+    for (let i = 1; i <= 100; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+        fibonacciArray[i] = n1;
+    }
+    if (fibNumb < 0) {
+        let oops = "OOPS";
+        return oops;
+    }
+    else {
+        return fibonacciArray[fibNumb];
+    } 
 };
 
 // Do not edit below this line
