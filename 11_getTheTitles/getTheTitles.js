eac2b4037cee329a367e67446ebe77d44c8ebf27
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-02-20 17:39:20 -0600

    Up to lesson 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return titles = books.map(books => books.title);
+    
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const year = new Date().getFullYear();
+    const oldestPerson = people.reduce((accumulator, currentValue) => {
+        if (((currentValue.yearOfDeath || year) - currentValue.yearOfBirth) > ((accumulator.yearOfDeath || year) - accumulator.yearOfBirth)) {
+          return currentValue;
+        }
+        else if (((currentValue.yearOfDeath || year) - currentValue.yearOfBirth) <= ((accumulator.yearOfDeath || year) - accumulator.yearOfBirth)) {
+          return accumulator;
+        }
+        
+        });
+    return oldestPerson;
 };
 
 // Do not edit below this line
